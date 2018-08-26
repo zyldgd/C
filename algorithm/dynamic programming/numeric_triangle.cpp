214ec16b7a32a6a7aef0dbd42311493fafb7f440
zing <1141822522@qq.com>
2018-08-26 21:07:21 +0800

    Update numeric_triangle.cpp

--- algorithm/dynamic programming/numeric_triangle.cpp
+++ algorithm/dynamic programming/numeric_triangle.cpp
@@ -1,4 +1,3 @@
-
 /*
 file   : numeric_triangle.cpp
 author : zyl
@@ -16,7 +15,6 @@ description:
     在上面的数字三角形中寻找一条从顶部到底边的路径，使得路径上所经过的数字之和最大。
     路径上的每一步都只能往左下或 右下走。只需要求出这个最大和即可，不必给出具体路径。
     三角形的行数大于1小于等于100，数字为 0-99
-    
 */
 
 #include <iostream>
@@ -57,7 +55,6 @@ int maxSum(int *arr, int size)
     delete[] M;
     return result;
 }
-
 
 
 int main()
