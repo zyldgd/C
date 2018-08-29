5bb8b6724e770869fea36584da26fe0d1e8131f9
zing <1141822522@qq.com>
2018-08-29 12:25:58 +0800

    Create trim.cpp

---
+++ STL/string/trim.cpp
@@ -0,0 +1,22 @@
+/*
+描述：
+>> trim("   123   ")
+   "123"
+
+
+相关依赖：
+#include <string>
+*/
+
+
+
+string trim(string str)
+{
+    if (str.empty())
+    {
+        return str;
+    }
+    str.erase(0, str.find_first_not_of(" "));
+    str.erase(str.find_last_not_of(" ") + 1);
+    return str;
+}
